9f6a93108a272739a71a781ded4f71ecb063d815
Mónica Bonito <monicasvbonito@agr1beja.pt>
2025-04-11 21:51:52 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    // Inicia o carrossel de personagens
-    $('#characterCarousel').carousel({
-        interval: 2000 // Intervalo de troca de slides
-    });
-
-    // Aqui você pode adicionar mais interações JavaScript, se necessário
-});

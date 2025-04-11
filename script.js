21973849ab69c5bdb3e9753b13e2ea533ac10553
Mónica Bonito <monicasvbonito@agr1beja.pt>
2025-04-11 20:56:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    // Inicia o carrossel de personagens
+    $('#characterCarousel').carousel({
+        interval: 2000 // Intervalo de troca de slides
+    });
+
+    // Aqui você pode adicionar mais interações JavaScript, se necessário
+});
